2272837378e9ce46035835a4c7e68650e1460f91
Joe <jwataru@gmail.com>
2016-12-28 10:30:25 +1100

    Implement creation of message.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,18 @@
 class MessagesController < ApplicationController
   def index
     @messages = Message.all
+    @message = Message.new
   end
+
+  def create
+    new_message = Message.new(message_params)
+    if new_message.save
+      render 'index'
+    end
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:content)
+    end
 end
