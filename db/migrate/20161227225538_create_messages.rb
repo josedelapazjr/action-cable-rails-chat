5e637763c8c031c9f00080024b470690cbebc86a
Joe <jwataru@gmail.com>
2016-12-30 21:13:08 +1100

    Add name in message model.

--- db/migrate/20161227225538_create_messages.rb
+++ db/migrate/20161227225538_create_messages.rb
@@ -1,6 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
+      t.text :name
       t.text :content
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20161227225538) do
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
+    t.text     "name"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
